3b51bd183ebc3727a1ed06e7cf1200fe6bbbca67
Saad kardad <saadkardad8@gmail.com>
2026-03-10 11:37:26 +0000

    ::::

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./section/Home";
 import About from "./section/AboutUs"
-import Contact from "./section/contact";
+import Contact from "./section/Contact";
 import Products from "./section/Products";
 
 export default function App() {
